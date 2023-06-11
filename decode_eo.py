8e495ca5ac2618190239cee45e0aaac3c0dcb3ed
InnokentiiHorikov <95141955+InnokentiiHorikov@users.noreply.github.com>
2023-06-11 18:17:27 +0300

    Update decode_eo.py

--- decode_eo.py
+++ decode_eo.py
@@ -1,1 +1,22 @@
+import numpy as np
 
+def decode_ep(img_name, enc_pix):
+  if enc_pix = '0':
+    return img_name, enc_pix
+  
+  else:
+    enc_pix = enc_pix.split()
+    end_point = np.len(enc_pix)
+    num_of_pix = enc_pix[::2]
+    rep_of_pix = enc_pix[1::2]
+    arr_of_pix = [np.arange(
+      np.arange(num_of_pix[i], num_of_pix[i] + rep_of_pix[i]))
+                  for i in range(lenght)]
+                  
+      
+    return img_name, np.concatenate(arr_of_pix)
+  
+
+  
+  
+  
