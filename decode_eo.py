63b48a9517ce569dd73207d009dcf8f45d31c3b6
InnokentiiHorikov <95141955+InnokentiiHorikov@users.noreply.github.com>
2023-06-11 18:24:27 +0300

    Update decode_eo.py

--- decode_eo.py
+++ decode_eo.py
@@ -1,8 +1,17 @@
 import numpy as np
 
+def to_mask(rle):
+  size = 768
+  rle -= 1
+  square = np.power(size, 2)
+  plot = np.zeros(square, dtype = np.uint8)
+  plot[rle] = 255
+  plot = np.reshape(plot, (width, height)).T
+  return plot
+
 def decode_ep(img_name, enc_pix):
   if enc_pix = '0':
-    return img_name, enc_pix
+    return img_name, np.zeroes((768,768))
   
   else:
     enc_pix = enc_pix.split()
@@ -12,9 +21,9 @@ def decode_ep(img_name, enc_pix):
     arr_of_pix = [np.arange(
       np.arange(num_of_pix[i], num_of_pix[i] + rep_of_pix[i]))
                   for i in range(lenght)]
-                  
-      
-    return img_name, np.concatenate(arr_of_pix)
+               
+
+    return img_name, to_mask(np.concatenate(arr_of_pix))
   
 
   
