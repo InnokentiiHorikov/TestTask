c785b90b5f0f8b8041d671d5bb80958bac89de9f
InnokentiiHorikov <95141955+InnokentiiHorikov@users.noreply.github.com>
2023-06-12 14:00:21 +0300

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -4,7 +4,32 @@ from keras import Sequential, Model
 from keras import layers
 from keras import Input
 
+def to_mask(rle):
+  size = 768
+  rle -= 1
+  square = np.power(size, 2)
+  plot = np.zeros(square, dtype = np.uint8)
+  plot[rle] = 255
+  plot = np.reshape(plot, (width, height)).T
+  return plot
 
+def decode_ep(img_name, enc_pix):
+  if enc_pix == '0':
+    return img_name, np.zeroes((768,768))
+  
+  else:
+    enc_pix = enc_pix.split()
+    end_point = np.len(enc_pix)
+    num_of_pix = enc_pix[::2]
+    rep_of_pix = enc_pix[1::2]
+    arr_of_pix = [np.arange(
+      np.arange(num_of_pix[i], num_of_pix[i] + rep_of_pix[i]))
+                  for i in range(lenght)]
+               
+
+    return img_name, to_mask(np.concatenate(arr_of_pix))
+  
+    
 def train_valid(data, split_point):
     import os, shutil
     
